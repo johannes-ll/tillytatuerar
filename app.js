a5f9756b01a344f65934202db45857f712be309d
johannes <45330378+johannes-ll@users.noreply.github.com>
2025-05-26 03:05:45 +0200

    Add files via upload
    Signed-off-by: johannes <45330378+johannes-ll@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const text = "tatuerare i uppsala, här är mitt portfolio."
 const textarray = text.split('')
 const maintext = document.querySelector('#maintext')
+const stars = document.querySelector('#stars')
 
 textarray.forEach((c, i) => {
     setTimeout(() => {
         maintext.innerHTML += c
     }, 50 * i)
-});
+})
+
+const star = ['★☆☆☆☆', '★★☆☆☆', '★★★☆☆', '★★★★☆', '★★★★★', '★★★★★', '★★★★★', '☆☆☆☆☆', '★★★★★', '☆☆☆☆☆', '★★★★★', '★★★★★', '★★★★★', '']
+
+let index = 0
+
+setInterval(() => {
+stars.innerHTML = star[index]
+index = (index + 1) % star.length
+}, 500)
